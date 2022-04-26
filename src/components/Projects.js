6e74d0ed3ee4146ca3166e991006cd48aa5d7813
john-chang94 <j.chang94@yahoo.com>
2022-04-26 13:53:35 -0700

    update projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,53 +1,50 @@
 import React, { useState } from "react";
-import quickSchedulesGif from "../images/coffee-logo.jpg";
+import quickSchedulesMockup from "../images/mockup-quick-schedules.png";
+import monchMockup from "../images/mockup-monch.png";
+import cryptoNetMockup from "../images/mockup-cryptonet.png";
 
 export const Projects = () => {
   return (
     <section id="projects">
       <p className="text-title text-center">Personal Projects</p>
       <div className="project-section grid">
-        <div className="l2-offset-3">
+        <div className="project-info l4-offset-2">
           <p className="text-subtitle">Quick Schedules</p>
           <p className="text-regular">
-            An employee management system with exclusive administrative and
+            Employee management system with exclusive administrative and
             employee privileges.
           </p>
         </div>
-        <div className="l3-offset-6">
+        <div className="l5-offset-6">
           <img
-            src={quickSchedulesGif}
+            src={quickSchedulesMockup}
             className="project-img"
             alt="project gif"
           />
         </div>
       </div>
       <div className="project-section grid">
-        <div className="l2-offset-3">
+        <div className="project-info l4-offset-2">
           <p className="text-subtitle">Monch!</p>
           <p className="text-regular">
-            An employee management system with exclusive administrative and
-            employee privileges.
+            Simple, easy-to-use, food review web application.
           </p>
         </div>
-        <div className="l3-offset-6">
-          <img
-            src={quickSchedulesGif}
-            className="project-img"
-            alt="project gif"
-          />
+        <div className="l5-offset-6">
+          <img src={monchMockup} className="project-img" alt="project gif" />
         </div>
       </div>
       <div className="project-section grid">
-        <div className="l2-offset-3">
+        <div className="project-info l4-offset-2">
           <p className="text-subtitle">CryptoNet</p>
           <p className="text-regular">
-            An employee management system with exclusive administrative and
-            employee privileges.
+            Cryptocurrency price tracker. View global market capitalization,
+            trading volume, and more.
           </p>
         </div>
-        <div className="l3-offset-6">
+        <div className="l5-offset-6">
           <img
-            src={quickSchedulesGif}
+            src={cryptoNetMockup}
             className="project-img"
             alt="project gif"
           />
