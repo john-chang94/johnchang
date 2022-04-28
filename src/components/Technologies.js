dc47039e47c1ea1b351f9379aab8a6f5c90f0e22
john-chang94 <j.chang94@yahoo.com>
2022-04-28 12:27:44 -0700

    update technologies

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -1,5 +1,89 @@
+import reactLogoBlack from "../images/logo-react-black.png";
+import awsS3LogoBlack from "../images/logo-aws-s3-black.png";
+import cssLogoBlack from "../images/logo-css-black.png";
+import expressLogoBlack from "../images/logo-express-black2.png";
+import firebaseLogoBlack from "../images/logo-firebase-black.png";
+import gitLogoBlack from "../images/logo-git-black.png";
+import githubLogoBlack from "../images/logo-github-black.png";
+import htmlLogoBlack from "../images/logo-html-black.png";
+import javaLogoBlack from "../images/logo-java-black.png";
+import jsLogoBlack from "../images/logo-javascript-black2.png";
+import es6LogoBlack from "../images/logo-es6-black.jpg";
+import mongoDbLogoBlack from "../images/logo-mongodb-black.png";
+import nodeLogoBlack from "../images/logo-node-black.png";
+import psqlLogoBlack from "../images/logo-postgresql-black.png";
+import postmanLogoBlack from "../images/logo-postman-black.png";
+
 export const Technologies = () => (
-    <section id="technologies">
-        <p className="text-regular">Technologies</p>
-    </section>
-)
+  <section id="technologies">
+    <div className="tech-list">
+      <p className="text-regular"><strong>Technologies</strong></p>
+      <ul>
+        <li>
+          <p>HTML/CSS</p>
+          <img className="logo-black" src={htmlLogoBlack} alt="html logo" />
+          <img className="logo-black" src={cssLogoBlack} alt="css logo" />
+        </li>
+        <li>
+          <p>Javascript/ES6</p>
+          <img className="logo-black" src={jsLogoBlack} alt="javascript logo" />&nbsp;
+          <img className="logo-black" src={es6LogoBlack} alt="es6 logo" />
+        </li>
+        <li>
+          <p>React.js</p>
+          <img className="logo-black" src={reactLogoBlack} alt="react logo" />
+        </li>
+        <li>
+          <p>Node.js</p>
+          <img className="logo-black" src={nodeLogoBlack} alt="node logo" />
+        </li>
+        <li>
+          <p>Express.js</p>
+          <img className="logo-black" src={expressLogoBlack} alt="express logo" />
+        </li>
+        <li>
+          <p>PostgreSQL</p>
+          <img className="logo-black" src={psqlLogoBlack} alt="postgresql logo" />
+        </li>
+        <li>
+          <p>Firebase</p>
+          <img className="logo-black" src={firebaseLogoBlack} alt="firebase logo" />
+        </li>
+      </ul>
+    </div>
+    <div className="tech-list">
+      <p className="text-regular"><strong>Utilities</strong></p>
+      <ul>
+        <li>
+          <p>Git</p>
+          <img className="logo-black" src={gitLogoBlack} alt="git logo" />
+        </li>
+        <li>
+          <p>Github</p>
+          <img className="logo-black" src={githubLogoBlack} alt="github logo" />
+        </li>
+        <li>
+          <p>Postman</p>
+          <img className="logo-black" src={postmanLogoBlack} alt="postman logo" />
+        </li>
+      </ul>
+    </div>
+    <div className="tech-list">
+      <p className="text-regular"><strong>Exposure</strong></p>
+      <ul>
+        <li>
+          <p>Java</p>
+          <img className="logo-black" src={javaLogoBlack} alt="java logo" />
+        </li>
+        <li>
+          <p>MongoDB</p>
+          <img className="logo-black" src={mongoDbLogoBlack} alt="mongo db logo" />
+        </li>
+        <li>
+          <p>Amazon S3</p>
+          <img className="logo-black" src={awsS3LogoBlack} alt="amazon s3 logo" />
+        </li>
+      </ul>
+    </div>
+  </section>
+);
